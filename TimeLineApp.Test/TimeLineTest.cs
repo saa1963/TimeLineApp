a1839897070f5a4aaad1498e57ce3bd7f22626aa
Андрей Сошин <a_soshin@mail.ru>
2018-11-23 16:53:16 +0300

    222

--- TimeLineApp.Test/TimeLineTest.cs
+++ TimeLineApp.Test/TimeLineTest.cs
@@ -50,5 +50,41 @@ namespace TimeLineApp.Test
             Assert.Null(o.Month);
             Assert.Null(o.Year);
         }
+        [Fact]
+        public void EventYearTest()
+        {
+            var o = new EventYear("11", 1);
+            Assert.Equal("11", o.Name);
+            Assert.Equal(1, o.Century);
+            Assert.Null(o.Day);
+            Assert.Equal(1, o.Decade);
+            Assert.Null(o.Month);
+            Assert.Equal(1, o.Year);
+            o = new EventYear("11", -1);
+            Assert.Equal("11", o.Name);
+            Assert.Equal(-1, o.Century);
+            Assert.Null(o.Day);
+            Assert.Equal(-1, o.Decade);
+            Assert.Null(o.Month);
+            Assert.Equal(-1, o.Year);
+        }
+        [Fact]
+        public void EventMonthTest()
+        {
+            var o = new EventMonth("11", 13);
+            Assert.Equal("11", o.Name);
+            Assert.Equal(1, o.Century);
+            Assert.Null(o.Day);
+            Assert.Equal(1, o.Decade);
+            Assert.Equal(13, o.Month);
+            Assert.Equal(2, o.Year);
+            o = new EventMonth("11", -13);
+            Assert.Equal("11", o.Name);
+            Assert.Equal(-1, o.Century);
+            Assert.Null(o.Day);
+            Assert.Equal(-1, o.Decade);
+            Assert.Equal(-13, o.Month);
+            Assert.Equal(-2, o.Year);
+        }
     }
 }
--- TimeLineApp/Models/TimeLine.cs
+++ TimeLineApp/Models/TimeLine.cs
@@ -6,19 +6,66 @@ using System.Threading.Tasks;
 
 namespace TimeLineApp.Models
 {
-    enum enPeriod
+    public struct Date
     {
-        day,month,year,decade,century
+        int Day;
+        int Month;
+        int Year;
+
+        public Date(int year, int month, int day)
+        {
+            if (year == 0) throw new ArgumentOutOfRangeException("year");
+            if (month > 12 || month < 1) throw new ArgumentOutOfRangeException("month");
+            if (day < 1) throw new ArgumentOutOfRangeException("day");
+            if ((new int[] { 1, 3, 5, 7, 8, 10, 12 }).Contains(month))
+            {
+                if (day > 31) throw new ArgumentOutOfRangeException("day");
+            }
+            else if ((new int[] { 4, 6, 9, 11 }).Contains(month))
+            {
+                if (day > 30) throw new ArgumentOutOfRangeException("day");
+            }
+            else
+            {
+                if (day > 29) throw new ArgumentOutOfRangeException("day");
+                if (year >= 1 && year <= 9999)
+                {
+                    var dt = new DateTime(year, month, day);
+                    if (DateTime.IsLeapYear(dt.Year))
+                    {
+                        if (day > 27) throw new ArgumentOutOfRangeException("day");
+                    }
+                    else
+                    {
+                        if (day > 28) throw new ArgumentOutOfRangeException("day");
+                    }
+                }
+            }
+            Day = day;
+            Month = month;
+            Year = year;
+        }
     }
     public class TimeLine
     {
-        //private 
+        private List<Event> m_list = new List<Event>();
+        public string Name { get; set; }
+
+        public TimeLine(string name)
+        {
+            Name = name;
+        }
+
+        public void Add(Event e)
+        {
+            m_list.Add(e);
+        }
     }
 
     public class Event
     {
         private string m_name;
-        private DateTime? m_day;
+        private Date? m_day;
         private int? m_month;
         private int? m_year;
         private int? m_decade;
@@ -27,14 +74,37 @@ namespace TimeLineApp.Models
         public Event(string name)
         {
             Name = name;
-            //Month = (dt.Year - 1) * 12 + dt.Month;
-            //Year = dt.Year;
-            //Decade = dt.Year / 10 + 1;
-            //Century = dt.Year / 100 + 1;
+        }
+
+        protected int CenturyFromDecade(int decade)
+        {
+            return decade / 10 + (decade / Math.Abs(decade));
+        }
+
+        protected int DecadeFromYear(int year)
+        {
+            return year / 10 + (year / Math.Abs(year));
+        }
+
+        protected int YearFromMonth(int month)
+        {
+            return (month - 1) / 12 + (month / Math.Abs(month));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month">1-12</param>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        protected int MonthFromDay(int year, int month, int day)
+        {
+            return 0;
         }
 
         public string Name { get => m_name; set => m_name = value; }
-        public DateTime? Day { get => m_day; set => m_day = value; }
+        public Date? Day { get => m_day; set => m_day = value; }
         public int? Month { get => m_month; set => m_month = value; }
         public int? Year { get => m_year; set => m_year = value; }
         public int? Decade { get => m_decade; set => m_decade = value; }
@@ -72,7 +142,66 @@ namespace TimeLineApp.Models
         public EventDecade(string name, int decade) : base(name)
         {
             Decade = decade;
-            Century = decade / 10 + 1;
+            Century = CenturyFromDecade(decade);
+        }
+    }
+
+    public class EventYear: Event
+    {
+        public EventYear(string name, int year): base(name)
+        {
+            Year = year;
+            Decade = DecadeFromYear(year);
+            Century = CenturyFromDecade(Decade.Value);
+        }
+    }
+
+    public class EventMonth: Event
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="year"></param>
+        /// <param name="month">месяц от 1 до 12</param>
+        public EventMonth(string name, int year, int month):base(name)
+        {
+            Month = ((Math.Abs(year) - 1) * 12 + month) * (year / Math.Abs(year));
+            Year = year;
+            Decade = DecadeFromYear(year);
+            Century = CenturyFromDecade(Decade.Value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="month">-1 ... +1</param>
+        public EventMonth(string name, int month):base(name)
+        {
+            Month = month;
+            Year = YearFromMonth(month);
+            Decade = DecadeFromYear(Year.Value);
+            Century = CenturyFromDecade(Decade.Value);
+        }
+    }
+
+    public class EventDay: Event
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="year"></param>
+        /// <param name="month">1-12</param>
+        /// <param name="day"></param>
+        public EventDay(string name, int year, int month, int day):base(name)
+        {
+            Day = new Date(year, month, day);
+            Month = ((Math.Abs(year) - 1) * 12 + month) * (year / Math.Abs(year));
+            Year = year;
+            Decade = DecadeFromYear(year);
+            Century = CenturyFromDecade(Decade.Value);
         }
     }
 }
