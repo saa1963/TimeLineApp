42d9c9a2f187bd32e733b92d4300c2efe4cbe67c
Андрей Сошин <a_soshin@mail.ru>
2018-12-20 22:11:49 +0300

    345

---
+++ TimeLineApp/wwwroot/tests/DateUtils.spec.ts
@@ -0,0 +1,14 @@
+﻿import { DateUtils } from '../src/dateutils';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('YMD->Number->YMD', () => {
+
+  it('should return {year:1963, month:6, day:5}', () => {
+    const result = DateUtils.YMDFromAD(DateUtils.DaysFromAD(1963,6,5))
+    expect(result.year).to.equal(1963)
+    expect(result.month).to.equal(6)
+    expect(result.day).to.equal(5)
+  });
+
+});
