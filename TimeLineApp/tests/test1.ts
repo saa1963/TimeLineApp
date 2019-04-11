3836dc19caaa9039f299865e9fdbb85bd9ead6f6
Андрей Сошин <a_soshin@mail.ru>
2019-04-11 22:53:49 +0300

    111

---
+++ TimeLineApp/tests/test1.ts
@@ -0,0 +1,12 @@
+﻿import { DateUtils } from '../wwwroot/src/dateutils';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello world!');
+  });
+
+});
