9c6e1279f5dd53ced687d2addd80017d826efaf8
Андрей Сошин <a_soshin@mail.ru>
2019-10-29 20:22:00 +0300

    121212

--- TimeLineApp/wwwroot/src/AddPeriod/AddPeriodView.ts
+++ TimeLineApp/wwwroot/src/AddPeriod/AddPeriodView.ts
@@ -1,1 +1,76 @@
-﻿
+﻿import { IAddPeriodView } from "./IAddPeriodView";
+import { EnumPeriod } from "../TLEvent"
+import { AddPeriodPresenter } from "./AddPeriodPresenter";
+
+export class AddPeriodView implements IAddPeriodView {
+  private Presenter: AddPeriodPresenter
+  private tbLogin: HTMLInputElement
+  private btnOk: HTMLButtonElement
+  private btnCancel: HTMLButtonElement
+  private dlg: HTMLElement
+  public constructor() {
+    
+  }
+  ShowDialog(): void {
+    throw new Error("Method not implemented.");
+  }
+  SetName(value: string): void {
+    throw new Error("Method not implemented.");
+  }
+  SetIsPeriod(value: boolean): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBeginType(value: EnumPeriod): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_DayDay(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_DayMonth(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_DayYear(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_MonthMonth(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_MonthYear(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetBegin_Year(value: number): void {
+    throw new Error("Method not implemented.");
+  }
+  SetError(err: string): void {
+    throw new Error("Method not implemented.");
+  }
+  GetName(): string {
+    throw new Error("Method not implemented.");
+  }
+  GetIsPeriod(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  GetBeginType(): EnumPeriod {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_DayDay(): number {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_DayMonth(): number {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_DayYear(): number {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_MonthMonth(): number {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_MonthYear(): number {
+    throw new Error("Method not implemented.");
+  }
+  GetBegin_Year(): number {
+    throw new Error("Method not implemented.");
+  }
+
+
+}
---
+++ TimeLineApp/wwwroot/src/AddPeriod/IAddPeriodView.ts
@@ -0,0 +1,24 @@
+﻿import { EnumPeriod } from "../TLEvent";
+
+export interface IAddPeriodView {
+  ShowDialog(): void
+  SetName(value: string): void
+  SetIsPeriod(value: boolean): void
+  SetBeginType(value: EnumPeriod): void
+  SetBegin_DayDay(value: number): void
+  SetBegin_DayMonth(value: number): void
+  SetBegin_DayYear(value: number): void
+  SetBegin_MonthMonth(value: number): void
+  SetBegin_MonthYear(value: number): void
+  SetBegin_Year(value: number): void
+  SetError(err: string): void
+  GetName(): string
+  GetIsPeriod(): boolean
+  GetBeginType(): EnumPeriod
+  GetBegin_DayDay(): number
+  GetBegin_DayMonth(): number
+  GetBegin_DayYear(): number
+  GetBegin_MonthMonth(): number
+  GetBegin_MonthYear(): number
+  GetBegin_Year(): number
+}
